78c9c7f3a10102dfcb27be932e6c282dd3ec244f
LBinin <lb905049701@qq.com>
2017-05-20 07:46:30 +0800

    添加统计代码、解决横屏适应问题

--- js/index.js
+++ js/index.js
@@ -1,31 +1,20 @@
 $(function(){
     // 设定高度
-    var bodyHeight = $(window).height() + 'px'
-    var bodyResize = false
+    var bodyHeight = $(window).height()
+    var bodyWidth = $(window).width()
+    if (bodyHeight <= bodyWidth) {
+        bodyHeight = $(window).width()
+        bodyWidth = $(window).height()
+    }
     $('.container').height(bodyHeight)
+    orientation_landscape()
+    $(window).resize(function(){
+        orientation_landscape()
+    })
+
+    function orientation_landscape () {
+        $('.container').height( bodyHeight / bodyWidth * $(window).width() )
+        $('input').height( bodyHeight / bodyWidth * $(window).width() * 0.06 )
+        $('select').height( bodyHeight / bodyWidth * $(window).width() * 0.06 )
+    }
 })
-// document.body.style.height = bodyHeight
-
-// 横屏
-// tip()
-// $(window).resize(function(){
-//     console.log($(window).height());
-//     tip()
-// })
-
-// function tip() {
-//     alert(window.orientation)
-//     if(window.orientation == 90 || window.orientation == -90) {
-//         document.body.style.height = $(window).height() + 'px'
-//         $('.orientation_landscape').css('visibility', 'visible')
-
-//     }else if(window.orientation == 180 || window.orientation == 0) {
-//         if (!bodyResize) {
-//             bodyHeight = $(window).height() + 'px'
-//             bodyResize = true
-//         }
-//         document.body.style.height = bodyHeight
-//         $('.orientation_landscape').css('visibility', 'hidden')
-
-//     }
-// }
